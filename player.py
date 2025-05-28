2270019ca0bb945fe6b8eb28b4211aa9db765b32
bangoim <joaocbbc@outlook.com>
2025-05-28 00:24:03 -0300

    create shot class

--- player.py
+++ player.py
@@ -1,14 +1,17 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -17,6 +20,7 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -29,10 +33,19 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
